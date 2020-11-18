f13793f3046fbfdb6cbba8b88a2a833e89657604
SkyWorldMC <73902956+SkyWorldMC@users.noreply.github.com>
2020-11-18 15:32:45 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "token": "Nzc4NjIwMzg2ODE5NTcxNzQ3.X7Uovg.l7t6wBPPVd_DmbFcFP9-NxSGG0M",
+    "token": "",
     "prefix": "!",
     "owners": ["660386189819510828"],
     "host": "https://skyworldbot.herokuapp.com",
